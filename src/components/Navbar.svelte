ef4d52b32be9851a7be8ba56672597da8770b756
Gataitai <mpattipeilohy1@gmail.com>
2026-03-29 13:21:58 +0800

    added banner

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import logo from '$lib/assets/logo.png';
+</script>
+
+<style>
+    .navbar {
+
+        font-size: 20px;
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        width: 100vw;
+        height: 7rem;
+
+        padding: 0 10rem 0 10rem;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+
+        font-family: 'Poppins', sans-serif;
+        gap: 5rem;
+        z-index: 10;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    .logo {
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+
+    .logo img {
+        height: 7rem;
+        width: auto;
+        display: block;
+    }
+</style>
+
+<nav class="navbar">
+    <a class="logo" href="/">
+        <img src={logo} alt="Cubulan logo" />
+    </a>
+
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/programme">Programme</a>
+    <a href="/signup">Signup</a>
+    <a href="/contact">Contact</a>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import banner from '$lib/assets/banner.png';
+</script>
+
+<style>
+    .page {
+        width: 100vw;
+    }
+
+    .section {
+        width: 100vw;
+        height: 100vh;
+        display: block;
+    }
+
+    .banner {
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+    .blue-dark {
+        background-color: var(--blue-dark);
+    }
+
+    .green {
+        background-color: var(--green);
+    }
+</style>
+
+<div class="page">
+    <section
+            class="section banner"
+            style:background-image={`url(${banner})`}
+    ></section>
+
+    <section class="section blue-dark">g</section>
+    <section class="section green">g</section>
+    <section class="section blue-dark">g</section>
+</div>
